75bcd9eca8238dee7efb9b9bd00fd2f9a63fa7f1
Daniel Park <abideinhope@gmail.com>
2018-06-10 08:28:46 +0900

    Added GET Posts with ID Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -21,9 +21,18 @@ router.get('/', (req, res) => {
   Post.find()
     .sort({date: -1})
     .then(posts => res.json(posts))
-    .catch(err => res.status(404));
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found'}));
 });
 
+// @route  GET api/posts/:id
+// @desc   Get post by id
+// @access Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({nopostsfount: 'No post found with that ID'})
+    );
+});
 
 // @route  POST api/posts
 // @desc   Create post
